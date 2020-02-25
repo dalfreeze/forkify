f9c2b1eeeddf84cadbe96a82cfc9eb4e55c14c05
dalfreeze <bendalferes@gmail.com>
2020-02-25 01:50:12 -0600

    set up complete

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,6 @@
 
 import num from './test'; // num will be the 23 exported from test.js. When importing from a module, you don't need the file extension.
 
-console.log(`I imported ${num} from another module.`);
+const x = 23;
+
+console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const htmlWebpackPlug = require('html-webpack-plugin');
 
 module.exports = {
     entry: ['./src/js/index.js'],
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new htmlWebpackPlug({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
