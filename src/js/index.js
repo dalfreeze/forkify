25dc28d9c9e016264bdce1dc53794eb0cb4500f2
dalfreeze <bendalferes@gmail.com>
2020-02-25 21:54:01 -0600

    connected forkify API, requests work

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,16 @@
 // import * as searchView from './views/searchView' // the * is everything
 
 // console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 2)}. On, and here you go: ${str}.`)
+
+import axios from 'axios';
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`); //returns a promise
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        console.log(error);
+    };
+};
+getResults('pizza');
